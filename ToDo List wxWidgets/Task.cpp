d1342260d74bff8ecc9653c4c6d33a902d7c7390
Kirtinos <Xrhstos_nt@yahoo.gr>
2024-06-11 19:12:31 +0300

    yippie

--- ToDo List wxWidgets/Task.cpp
+++ ToDo List wxWidgets/Task.cpp
@@ -10,13 +10,38 @@ void saveTasksToFile(const std::vector<Task>& tasks, const std::string& fileName
 	std::ofstream ostream(fileName);
 	ostream << tasks.size();
 
+	//loop over all the tasks
 	for (const Task& task : tasks)
 	{
 		std::string description = task.description;
+		std::replace(description.begin(), description.end(), ' ', '_');
+
+		ostream << '\n' << description << ' ' << task.done;
 	}
 }
 
 std::vector<Task> loadTasksFromFile(const std::string& fileName)
 {
-	return std::vector<Task>();
+	if (!std::filesystem::exists(fileName))
+	{
+		return std::vector<Task>();
+	}
+
+	std::vector<Task> tasks;
+	std::ifstream istream(fileName);
+
+	int n;
+	istream >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		std::string description;
+		bool done;
+
+		istream >> description >> done;
+		std::replace(description.begin(), description.end(), '_', ' ');
+		tasks.push_back(Task{ description, done });
+	}
+
+	return tasks;
 }
